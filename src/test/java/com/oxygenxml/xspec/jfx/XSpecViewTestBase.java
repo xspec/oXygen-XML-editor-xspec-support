eafa5f6ed50ab76af65c44c506c2189461096b05
AlexJitianu <alex_jitianu@sync.ro>
2017-12-19 12:48:05 +0200

    Slightly different command line for running ant.

--- src/test/java/com/oxygenxml/xspec/jfx/XSpecViewTestBase.java
+++ src/test/java/com/oxygenxml/xspec/jfx/XSpecViewTestBase.java
@@ -268,7 +268,8 @@ public class XSpecViewTestBase extends JFCTestCase {
     File ant = URLUtil.getCanonicalFileFromFileUrl(getClass().getClassLoader().getResource("cmd/apache-ant-1.10.1/lib/ant.jar"));
 
     StringBuilder antlauncherJar = new StringBuilder();
-    antlauncherJar.append(antL.getAbsolutePath()).append(";").append(ant.getAbsolutePath());
+    antlauncherJar.append("\"").append(antL.getAbsolutePath()).append("\"").append(";")
+    .append("\"").append(ant.getAbsolutePath()).append("\"");
     
     // Build the classpath needed by the build_report.xml script.
     StringBuilder cl = new StringBuilder();
@@ -294,7 +295,7 @@ public class XSpecViewTestBase extends JFCTestCase {
     
     String cmd = 
         "java -Xmx256m "
-        + "-classpath \"" + antlauncherJar.toString() + "\" "
+        + "-classpath " + antlauncherJar.toString() + " "
         + "org.apache.tools.ant.launch.Launcher "
         // Classpath
         + cl.toString()
