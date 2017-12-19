9e6c1e2c8c989e9d4f0b7431c31e46a707e46aec
Alex Jitianu <alex_jitianu@sync.ro>
2017-12-19 11:38:15 +0200

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,26 +4,16 @@ REPONAME=`basename $PWD`
 PARENTDIR=`dirname $PWD`
 USERNAME=`basename $PARENTDIR`
 
-mkdir deploy
-cd deploy
-
-echo "Initialize repository"
-
-git init
 git config user.name "AlexJitianu";
 git config user.email "alex_jitianu@oxygenxml.com";
 
-echo "Link with https://$GH_TOKEN@github.com/$USERNAME/$REPONAME.git"
+echo "Check out https://$GH_TOKEN@github.com/$USERNAME/$REPONAME.git"
 
-git remote add upstream "https://$GH_TOKEN@github.com/$USERNAME/$REPONAME.git"
-git fetch upstream
-git reset upstream/master
+git clone "https://$GH_TOKEN@github.com/$USERNAME/$REPONAME.git"
+cd $REPONAME
 
 cp -f ../target/update_site.xml build;
 
-ls
-git status
-
 git add build/update_site.xml;
 git commit -m "New addon release - ${TRAVIS_TAG}";
-git push -q upstream HEAD:master;
+git push
