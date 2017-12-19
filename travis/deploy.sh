f185454fb610624e089683a88c76bb7bddbf644e
Alex Jitianu <alex_jitianu@sync.ro>
2017-12-19 10:45:36 +0200

    Try a new deploy script.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,9 +1,23 @@
+
+REPONAME=`basename $PWD`
+PARENTDIR=`dirname $PWD`
+USERNAME=`basename $PARENTDIR`
+
+mkdir deploy
+cd deploy
+git init
 git config user.name "AlexJitianu";
 git config user.email "alex_jitianu@oxygenxml.com";
-git fetch;
-git checkout master;
-git reset;
-cp -f target/update_site.xml build;
+
+git remote add upstream "https://$GH_TOKEN@github.com/$USERNAME/$REPONAME.git"
+git fetch upstream
+git reset upstream/master
+
+cp -f ../target/update_site.xml build;
+
+ls
+git status
+
 git add build/update_site.xml;
 git commit -m "New addon release - ${TRAVIS_TAG}";
-git push origin HEAD:master; 
+git push -q upstream HEAD:master;
